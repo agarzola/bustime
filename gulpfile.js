febb7fb694687cfeba32c11fff0c7d606c90b458
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2014-11-26 10:06:54 -0500

    Lint gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -19,21 +19,21 @@ var gulp = require('gulp'),
  * introduced a feature or made a backwards-incompatible release.
  */
 
-function inc(importance) {
-    // get all the files to bump version in
-    return gulp.src(['./package.json'])
-        // bump the version number in those files
-        .pipe(bump({type: importance}))
-        // save it back to filesystem
-        .pipe(gulp.dest('./'))
-        // commit the changed version number
-        .pipe(git.commit('bump package version'))
-        // read only one file to get the version number
-        .pipe(filter('package.json'))
-        // **tag it in the repository**
-        .pipe(tag_version());
+function inc (importance) {
+  // get all the files to bump version in
+  return gulp.src(['./package.json'])
+    // bump the version number in those files
+    .pipe(bump({type: importance}))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // commit the changed version number
+    .pipe(git.commit('bump package version'))
+    // read only one file to get the version number
+    .pipe(filter('package.json'))
+    // **tag it in the repository**
+    .pipe(tag_version());
 }
 
-gulp.task('patch', function() { return inc('patch'); })
-gulp.task('feature', function() { return inc('minor'); })
-gulp.task('release', function() { return inc('major'); })
+gulp.task('patch', function () { return inc('patch'); })
+gulp.task('feature', function () { return inc('minor'); })
+gulp.task('release', function () { return inc('major'); })
