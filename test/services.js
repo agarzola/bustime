f15783557bb83fa53ef846511ce927c61534d160
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2014-11-27 16:04:49 -0500

    Add tests for services

---
+++ test/services.js
@@ -0,0 +1,52 @@
+var assert = require('assert'),
+    should = require('should'),
+    services = require('../lib/modules/services'),
+    result, reqObj
+    ;
+
+// SERVICES
+describe('Services:', function () {
+  before(function () {
+    mockResult = {
+      tm: '20141201 10:58',
+      prd:{
+        tmstmp: '20141201 10:58',
+        prdtm: '20141201 11:01'
+      }
+    }
+    reqObj = {
+      services: {
+        calculateETA: true,
+        moment: true
+      }
+    }
+  });
+
+  describe('calculateETA:', function () {
+    it('should produce an `eta` property', function (done) {
+      services.predictions(reqObj, mockResult, function (err, processed) {
+        if (err) done(err);
+        processed.prd.should.have.property('eta');
+        done();
+      });
+    });
+
+    it('should calculate 3m in milliseconds (180000)', function (done) {
+      services.predictions(reqObj, mockResult, function (err, processed) {
+        if (err) done(err);
+        processed.prd.eta.should.equal(180000);
+        done();
+      });
+    });
+  });
+
+  describe('moment:', function () {
+    it('should produce a `moment` property', function (done) {
+      services.time(reqObj, mockResult, function (err, processed) {
+        if (err) done(err);
+        processed.should.have.property('moment');
+        done();
+      });
+    });
+  });
+});
