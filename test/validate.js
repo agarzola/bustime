78846f5cab40732c88ce71524f3228e08667d82f
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2014-11-27 17:23:44 -0500

    Improve validation testing; add forbidden property test

--- test/validate.js
+++ test/validate.js
@@ -1,42 +1,62 @@
-var assert = require('assert'),
-    should = require('should'),
+var assert   = require('assert'),
+    should   = require('should'),
+    Joi      = require('joi'),
     validate = require('../lib/modules/validate'),
-    reqObj
+    methods  = [ 'time', 'vehicles', 'routes', 'directions',
+                'stops', 'patterns', 'predictions', 'serviceBulletins' ],
+    schema   = Joi.object().keys({ notBogusProperty: Joi.any() }),
+    reqObj, reqObjBypass, schema
     ;
 
 // VALIDATION
 describe('Validation:', function () {
-  describe('bogus property:', function () {
-    before(function () {
-      reqObj = {
-        bogusProperty: '123'
-      }
-    });
 
-    it('time should reject bogus property', function (done) {
-      validate.time(reqObj, function (err, validatedObj) {
+  before(function () {
+    reqObj = {
+      bogusProperty: '123'
+    }
+    reqObjBypass = {
+      bogusProperty: '123',
+      services: {
+        validate: false
+      }
+    }
+  });
+
+  describe('general validation:',function () {
+    // All validation methods delegate to generic .validation() method
+    it('validate.validation() should reject bogus property', function (done) {
+      validate.validation(reqObj, schema, function (err, validatedObj) {
         should.exist(err);
         done();
       });
     });
-  });
 
-  describe('validation bypass:', function () {
-    before(function () {
-      reqObj = {
-        bogusProperty: '123',
-        services: {
-          validate: false
-        }
-      }
-    });
-
-    it('time should ignore bogus property', function (done) {
-      validate.time(reqObj, function (err, validatedObj) {
+    // All validation methods delegate to generic .validation() method
+    it('validate.validation() method should ignore bogus property', function (done) {
+      validate.validation(reqObjBypass, schema, function (err, validatedObj) {
         if (err) return done(err);
         should.not.exist(err);
         done();
       });
     });
   });
+
+  describe('forbidden properties:', function () {
+
+    before(function () {
+      reqObj['format'] = 'json';
+    });
+
+    methods.forEach(function (method) {
+      it('validate.' + method + '() should reject forbidden `format` property', function (done) {
+        validate[method](reqObj, function (err, validatedObj) {
+          should.exist(err);
+          done();
+        });
+      });
+    });
+
+  });
+
 });
