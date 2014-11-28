1382ba84d16eb809689807c32c7da939a90cc105
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2014-11-28 11:43:18 -0500

    Improve test for collectRoutesAndStops()

--- test/utility.js
+++ test/utility.js
@@ -5,9 +5,16 @@ var should   = require('should'),
 
 describe('Utility', function () {
   it('should produce a collection of routes', function (done) {
-    utility.collectRoutes(function (err, result) {
+    utility.collectRoutesAndStops(function (err, result) {
       if (err) return done(err);
-      var schema = Joi.array().includes(Joi.object());
+      var schema = Joi.array().includes(Joi.object().keys({
+        rt: Joi.string().required(),
+        rtnm: Joi.string().required(),
+        rtclr: Joi.string().required(),
+        dir: Joi.array().includes(Joi.object().keys({
+          stops: Joi.array().includes(Joi.string()).required()
+        })).required()
+      }));
       Joi.validate(result, schema, function (err, validated) {
         done(err);
       });
